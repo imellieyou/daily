d16159d285220630b0e701f1a192cafc74dbb7c7
imellieyou <imellieyou@naver.com>
2024-03-17 16:49:49 +0900

    feat: 기본 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import Main from "./pages/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Dashboard, SignUp, LogIn, NotFound } from "./pages";
 
 const App = () => {
   return (
     <div>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<LogIn />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUp />}
+          />
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
